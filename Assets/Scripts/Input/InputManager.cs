7bc2a3a6286fa4bf22fa65e108780cd64a3b75cc
crimson-catfish <sava_kto@mail.ru>
2024-06-12 01:39:29 +0300

    refactor: remove unused OnCast action

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -5,10 +5,8 @@ public class InputManager : Singleton<InputManager>
 {
     public event Action<Vector2> OnNextDrawPosition;
 
-    // CAREFULLY! called only after OnNextDrawPosition somehow
     public event Action<Vector2> OnDrawStart;
     public event Action OnDrawEnd;
-    public event Action OnCast;
 
     private Controls controls;
     private readonly float screenWidth = Screen.width;
@@ -28,8 +26,6 @@ public class InputManager : Singleton<InputManager>
         controls.Touch.DrawContact.started += _ => OnDrawStart?.Invoke(controls.Touch.DrawPosition.ReadValue<Vector2>() / screenWidth);
         controls.Touch.DrawPosition.performed += context => OnNextDrawPosition?.Invoke(context.ReadValue<Vector2>() / screenWidth);
         controls.Touch.DrawContact.canceled += _ => OnDrawEnd?.Invoke();
-        
-        // controls.Touch.Cast.performed += _ => OnCast?.Invoke();
     }
 
     private void OnDisable()
