af1493098657824cea1f6ad57cba1dd64962a6d8
crimson-catfish <sava_kto@mail.ru>
2024-06-14 14:34:02 +0300

    refactor: minor

--- Assets/Scripts/RuneSystem/Rune.cs
+++ Assets/Scripts/RuneSystem/Rune.cs
@@ -1,12 +1,12 @@
-using UnityEngine;
 using System.Collections.Generic;
 using System;
+using UnityEngine;
 #if UNITY_EDITOR
-    using UnityEditor;
+using UnityEditor;
 #endif
 
 /// <summary>
-/// rune's width always 1, height is positive.
+/// rune's width always 1, height is positive float.
 /// </summary>
 [Serializable]
 public class Rune : ScriptableObject, IComparable<Rune>
@@ -16,20 +16,22 @@ public class Rune : ScriptableObject, IComparable<Rune>
     public string previewPath;
     private Texture2D preview;
 
-    #if UNITY_EDITOR
-        [property: SerializeField]
-        public Texture2D Preview
+#if UNITY_EDITOR
+    [property: SerializeField]
+    public Texture2D Preview
+    {
+        get
         {
-            get
-            {
-                if (preview == null) preview = AssetDatabase.LoadAssetAtPath<Texture2D>(previewPath);
-                return preview;
-            }
-            set => preview = value;
-        }
-    #endif
+            if (preview == null)
+                preview = AssetDatabase.LoadAssetAtPath<Texture2D>(previewPath);
 
-    public float averageHeight = 0;
+            return preview;
+        }
+        set => preview = value;
+    }
+#endif
+
+    public float averageHeight;
     public Vector2 averageMassCenter = Vector2.zero;
     public List<RuneDrawVariation> drawVariations = new();
 
