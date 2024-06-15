2d000edf108c1fc779ea9f98510bce408a7a0dd0
crimson-catfish <sava_kto@mail.ru>
2024-06-15 03:44:06 +0300

    feat: manually manage toggle graphics

--- Assets/Scripts/RuneSystem/Makers/RuneToggle.cs
+++ Assets/Scripts/RuneSystem/Makers/RuneToggle.cs
@@ -16,6 +16,37 @@ public class RuneToggle : MonoBehaviour
     }
 
     [SerializeField] private RawImage rawImage;
+    [SerializeField] private Toggle toggle;
+    [SerializeField] private Outline outline;
+
+
+    [Header("Transition color settings")] [SerializeField]
+    private Color normalColor = new(1f, 1f, 1f, 0.75f);
+
+    [SerializeField] private Color selectedColor = new(0.74f, 0.74f, 0.74f);
+    [SerializeField] private Color outlineColorSelected = new Color(0f, 0f, 0f, 0.82f);
+
 
     private Rune rune;
+
+    private void OnEnable()
+    {
+        rawImage.color = normalColor;
+        outline.effectColor = Color.clear;
+        toggle.onValueChanged.AddListener(HandleValueChange);
+    }
+
+    private void HandleValueChange(bool value)
+    {
+        if (value)
+        {
+            rawImage.color = selectedColor;
+            outline.effectColor = outlineColorSelected;
+        }
+        else
+        {
+            rawImage.color = normalColor;
+            outline.effectColor = Color.clear;
+        }
+    }
 }
