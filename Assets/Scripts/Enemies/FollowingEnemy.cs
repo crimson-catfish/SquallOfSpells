c75c19effb77ef7943bdc5c8092a92d7282dc205
crimson-catfish <sava_kto@mail.ru>
2024-06-27 15:01:16 +0300

    feat: add movement to following enemy

--- Assets/Scripts/Enemies/FollowingEnemy.cs
+++ Assets/Scripts/Enemies/FollowingEnemy.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public abstract class FollowingEnemy : Enemy
 {
+    [SerializeField] private float speed;
+
     protected void Follow()
     {
-        Vector3 direction = player.transform.position - transform.position;
+        Vector3 position = transform.position;
+
+        Vector3 direction = player.transform.position - position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if (direction.magnitude > 1f)
+            position += direction.normalized * (speed * Time.deltaTime);
+
+        transform.position = position;
     }
 }
