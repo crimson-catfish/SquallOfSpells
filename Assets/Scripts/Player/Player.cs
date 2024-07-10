f610598f1be423b68c586f42316820fceae822cc
crimson-catfish <sava_kto@mail.ru>
2024-07-10 22:40:02 +0300

    feat: look move direction

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -34,6 +34,10 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
+        if (moveDirection.sqrMagnitude == 0f)
+            return;
+        
+        LookTo(moveDirection);
         transform.position += moveDirection * Time.deltaTime;
     }
 
