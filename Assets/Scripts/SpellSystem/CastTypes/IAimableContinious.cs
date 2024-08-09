6d85058f4ccbacc02c63239ef5f691da5027c1fd
crimson-catfish <sava_kto@mail.ru>
2024-08-09 15:16:18 +0500

    refactor: rename from endurable to continuous

--- Assets/Scripts/SpellSystem/CastTypes/IAimableContinious.cs
+++ Assets/Scripts/SpellSystem/CastTypes/IAimableContinious.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace SquallOfSpells.SpellSystem
 {
-    public interface IEnduringAimable : IAimable
+    public interface IAimableContinious : IAimable
     {
         public void StartCasting(Vector2 direction);
         public void NewDirection(Vector2 direction);
