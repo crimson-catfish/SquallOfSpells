adee47842c09640e8506c012d3caf1c3b549c56f
crimson-catfish <sava_kto@mail.ru>
2024-12-04 15:38:33 +0300

    use autoaim in fireball spell

--- Assets/Scripts/SpellSystem/Spells/FireballSpell.cs
+++ Assets/Scripts/SpellSystem/Spells/FireballSpell.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 namespace SquallOfSpells.SpellSystem
 {
-    public class FireballSpell : MonoBehaviour
+    public class FireballSpell : MonoBehaviour, ICastable
     {
         [SerializeField] private GameObject fireballProjectile;
+        [SerializeField] private Aimer      aimer;
+
+        [SerializeField] private float autoAimRange = 5.0f;
 
 
-        public void Cast(Vector2 direction)
+        public void Cast()
         {
+            Vector2 direction = aimer.ClosestTarget(autoAimRange) - (Vector2)transform.position;
+
             Instantiate(fireballProjectile, this.transform.position,
                 Quaternion.Euler(
                     new Vector3(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg)));
